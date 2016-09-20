37306d2f6a5b39dc0e40801e5d0984d32bf58b09
corbrat89 <corbrat89@gmail.com>
2016-09-19 20:07:53 -0400

    Week 4 challenge code

--- Challenges/Week4/ChallengeFiles/js/main.js
+++ Challenges/Week4/ChallengeFiles/js/main.js
@@ -1,19 +1,26 @@
-function openBlock (id) {
-    var block = document.getElementById(id);
-    var blocks = document.getElementById("main_content").getElementsByTagName("div");
+$('#menu a').click(function(){ 
+	//split id to get last part for openBlock
+	var linkIDParts = this.id.split("_");
+	openBlock(linkIDParts[2]); 
+	return false; 
+});
 
-    for (var y = 0; y < blocks.length; y++) {
-        blocks[y].style.display = "none";
-    }
-    block.style.display = "block";
-    
+function openBlock (id) {    
+	$("#main_content div").hide();
+	$("#"+id).show();
+	
     // TODO: remove the "active" class from all of the li elements inside the menu
-    // TODO: add the "active" class to the li element that contains the link that was clicked
-
+    $("#menu li").removeClass("active");
+	// TODO: add the "active" class to the li element that contains the link that was clicked
+	$("#menu_item_"+id).parent().addClass("active");
 }
 
 // TODO: add the "hover" class to the menu items when you hover over them
-
+$("#menu a").hover(function() {
+	$(this).toggleClass("hover");
+});
 
 // TODO: set up the tooltip plugin on all of the links in the menu
-
+$('#menu a').tooltip({
+    delay: 1000
+});
