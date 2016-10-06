c827bd706af3e2147ca38bbd19d754db2e61c917
corbrat89 <corbrat89@gmail.com>
2016-10-05 22:16:14 -0400

    Week 6 solution update

--- Challenges/Week6/js/app.js
+++ Challenges/Week6/js/app.js
@@ -9,3 +9,36 @@
  
 //Extra credit
 //Create interactive buttons to decrease or increase the Width and Volume
+function box(){
+	this.height = 0;
+	this.width = 0;
+	this.volume = 0;
+	
+	this.adjust = function (adjustment, value)
+	{
+		if(this[value] == 0 && adjustment == -1){
+			alert(value + " cannot be negative");
+		}
+		else {
+			this[value] += adjustment;
+		}
+	};
+	
+	this.print = function(){
+		return JSON.stringify(this, null, 4);
+	};
+}
+
+var myBox = new box();
+
+//leverage data attributes to make only one function instead of one per button
+$('body').on('click', '.change', function() {
+	myBox.adjust($(this).data("adjust"), $(this).data("value"));
+	$("#Print").click();
+});
+
+$("#Print").click(function(){
+	$("#output").empty();
+	var str = myBox.print();
+	$("#output").append(str);
+});
