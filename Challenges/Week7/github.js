3363e618ec861973666cb0468797f630dbff5ea0
corbrat89 <corbrat89@gmail.com>
2016-10-12 23:59:09 -0400

    Added error handler for week 7 challenge

--- Challenges/Week7/github.js
+++ Challenges/Week7/github.js
@@ -1,26 +1,36 @@
-function getRepos(username){
-console.log('Repos for ' + username);
-var http = require("https");
-var req_options = 
-{
-	host: 'api.github.com',
-	headers: {'User-Agent': 'corbrat89'},
-	path: '/users/' + username + '/repos'
-};
 
-//connect to the api url
-var request = http.get(req_options, function(res){
-	var body = "";
-	res.on('data', function(chunk){
-		body += chunk;
+function getRepos(username){
+	console.log('Repos for ' + username);
+	var http = require("https");
+	var req_options = 
+	{
+		host: 'api.github.com',
+		headers: {'User-Agent': 'corbrat89'},
+		path: '/users/' + username + '/repos'
+	};
+
+	//connect to the api url
+	var request = http.get(req_options, function(res){
+		var body = "";
+		res.on('data', function(chunk){
+			body += chunk;
+		});
+		res.on('end', function(){
+			if(res.statusCode === 200) {
+				try {
+					var data = JSON.parse(body);
+					for(var key in data) {
+						console.log(data[key].name);
+					}
+				} catch(error){
+					console.error(error.message);
+				}
+			} else {
+				console.error("There was an error getting the github repos for " + username + ". (" +
+							res.statusCode + ")");
+			}
+		});
 	});
-	res.on('end', function(){
-		var data = JSON.parse(body);
-		for(var key in data) {
-			console.log(data[key].name);
-		}
-	});
-});
 }
 
 
